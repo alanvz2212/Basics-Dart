0d588734d09c217af99f63dfbacf14272e900a5a
Alan-varghese <alanvz2212@gmail.com>
2025-02-23 21:10:37 +0400

    Initial Commit

---
+++ datatypes_&_variables.dart
@@ -0,0 +1,16 @@
+//Datatypes and Variables
+
+void main() {
+  int age = 22;
+  double marks = 20.5;
+  String name = "Alan";
+  bool male = true;
+  List<int> nums = [1, 2, 3, 4, 5];
+  Set<String> fruits = {"Apple", "Mango"};
+  dynamic x = "Text";
+  {
+    print("the age is $age");
+    print("the marks is $marks");
+    print(male);
+  }
+}
