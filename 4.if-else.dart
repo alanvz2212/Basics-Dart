146abd968b45d9bf2a52dc5cbcb33d96b41f9a7e
Alan-varghese <alanvz2212@gmail.com>
2025-03-05 10:43:04 +0400

    Initial Commit

---
+++ 4.if-else.dart
@@ -0,0 +1,12 @@
+// if-else conditions
+
+// if you are above the age of 18, then you are eliglble to vote
+
+void main() {
+  int age = 17;
+  if (age >= 18) {
+    print("you are eligible to vote");
+  } else {
+    print("You are not eligible to vote");
+  }
+}
