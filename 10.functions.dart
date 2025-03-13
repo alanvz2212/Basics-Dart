1946d051dc8b10d13909beefd87615e8b8f894d5
Alan-varghese <alanvz2212@gmail.com>
2025-03-13 14:18:40 +0400

    Updated

--- 10.functions.dart
+++ 10.functions.dart
@@ -1,15 +1,37 @@
+// void main() {
+//   String result = FunctionName("Alan");
+//   print(result);
+
+//   int Value = FunctionnNumber(22);
+//   print(Value);
+// }
+
+// String FunctionName(String n) {
+//   return n + "Varghese";
+// }
+
+// int FunctionnNumber(int x) {
+//   return x;
+// }
+
 void main() {
-  String result = FunctionName("Alan");
+  // Alan alan =new Alan();
+
+  String result = Alan.FunctionName("Alan ");
   print(result);
-
-  int Value = FunctionnNumber(22);
-  print(Value);
+  int value = Alan.FunctionAge(20);
+  print(value);
 }
 
-String FunctionName(String n) {
-  return n + "Varghese";
+class Alan {
+  static FunctionName(String n) {
+    return n + "Varghese";
+  }
+
+  static FunctionAge(int s) {
+    return 2 * 100;
+  }
 }
 
-int FunctionnNumber(int x) {
-  return x;
-}
+// Object
+// static
