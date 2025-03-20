c1105669031c12dbed44c3712d8b58fd81c8c79a
Alan-varghese <alanvz2212@gmail.com>
2025-03-20 10:26:24 +0400

    Updated

--- 12.named_parameters.dart
+++ 12.named_parameters.dart
@@ -1,10 +1,1 @@
-void main() {
-  Map data = userdata("Alan", 21);
-  {
-    print(data);
-  }
-}
-
-Map userdata(String name, int age) {
-  return {"Name": name, "Age": age};
-}
+// named_parameters
