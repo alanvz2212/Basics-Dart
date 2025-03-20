055f3d25f231cfb31a98b62feaa9f4146349e2c2
Alan-varghese <alanvz2212@gmail.com>
2025-03-20 11:02:17 +0400

    Updated

--- 12.named_parameters.dart
+++ 12.named_parameters.dart
@@ -1,1 +1,10 @@
 // named_parameters
+
+void main() {
+  Map data = FunctionName(age: 22, name: "Alan");
+  print(data);
+}
+
+Map FunctionName({required String name, required int age}) {
+  return {"Name": name, "Age": age};
+}
