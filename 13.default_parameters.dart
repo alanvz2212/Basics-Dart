f99fba874ca0063966d7c94fe1a11675804d19c2
Alan-varghese <alanvz2212@gmail.com>
2025-03-23 16:53:54 +0400

    Updated

--- 13.default_parameters.dart
+++ 13.default_parameters.dart
@@ -1,17 +1,17 @@
 // // default Parameters
 
 void main() {
-  // double length = 12;
+  // double length = 10;
   // double breadth = 10;
 
   print(areaRectangle());
   print(areaCircle());
 }
 
-double areaRectangle({double length = 20, double breadth = 10}) {
+double areaRectangle({double length = 10, double breadth = 10}) {
   return length * breadth;
 }
 
-double areaCircle({double radius = 12}) {
+double areaCircle({double radius = 10}) {
   return 3.14 * radius * radius;
 }
