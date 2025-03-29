85fc61455622a5d051c6f5770ecf8ce150d7f126
Alan-varghese <alanvz2212@gmail.com>
2025-03-29 18:48:02 +0400

    Updated

---
+++ 18.inheritance_thoery.dart
@@ -0,0 +1,18 @@
+// heirarchial Inheritance
+
+// Mobile Phone extends Electronics- call(),games()
+// Television extends Electronics - 
+// Electronics - height ,width,thickness,watch,AspectRatio,watch()
+
+// Single Inheritance
+
+// Mobile Phone extends Electronics -
+// Electronics - height ,width,thickness,watch,AspectRatio,watch()
+
+
+// MultiLevel Inheritance
+
+
+// Television extends mobile Phone -
+// Mobile Phone extends Electronics -
+// Electronics - height ,width,thickness,watch,AspectRatio,watch()
