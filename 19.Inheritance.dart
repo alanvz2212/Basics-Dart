f5412da6e71f7efcb040049608923eaa05c00b4e
Alan-varghese <alanvz2212@gmail.com>
2025-03-29 21:57:27 +0400

    Updated

--- 19.Inheritance.dart
+++ 19.Inheritance.dart
@@ -3,6 +3,19 @@ void main() {
   mobilephone.height = 50;
   mobilephone.width = 100;
   mobilephone.thickness = 45;
+  mobilephone.printHeight();
+  mobilephone.printWidth();
+  mobilephone.printthickness();
+  mobilephone.playGames();
+  mobilephone.callContact();
+
+  TeleVision television = new TeleVision();
+  television.height = 20;
+  television.width = 30;
+  television.thickness = 100;
+  television.printHeight();
+  television.printWidth();
+  television.printthickness();
 }
 
 class Mathematics {
@@ -12,6 +25,18 @@ class Mathematics {
 
   void wath() {
     print("Electronics Item is being Watched");
+  }
+
+  void printHeight() {
+    print("Electronics item is being Watched=$height");
+  }
+
+  void printWidth() {
+    print("Height of Item = $height");
+  }
+
+  void printthickness() {
+    print("thickness of item = $thickness");
   }
 }
 
@@ -24,3 +49,5 @@ class MobilePhone extends Mathematics {
     print("Calling through mobile phone");
   }
 }
+
+class TeleVision extends Mathematics {}
