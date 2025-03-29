4089d4203e3e710ef81c418a1a224cae8a8c6522
Alan-varghese <alanvz2212@gmail.com>
2025-03-29 19:50:33 +0400

    Updated

--- 19.Inheritance.dart
+++ 19.Inheritance.dart
@@ -1,1 +1,26 @@
-void main() {}
+void main() {
+  MobilePhone mobilephone = MobilePhone();
+  mobilephone.height = 50;
+  mobilephone.width = 100;
+  mobilephone.thickness = 45;
+}
+
+class Mathematics {
+  double height = 5;
+  double width = 2;
+  double thickness = 10;
+
+  void wath() {
+    print("Electronics Item is being Watched");
+  }
+}
+
+class MobilePhone extends Mathematics {
+  void playGames() {
+    print("Play Games on the Mobile phones");
+  }
+
+  void callContact() {
+    print("Calling through mobile phone");
+  }
+}
