b409996a9b8253fd5470d6fecd9022f7f6730980
Alan-varghese <alanvz2212@gmail.com>
2025-03-29 12:15:22 +0400

    Updated

---
+++ 16.getter_and_setter.dart
@@ -0,0 +1,33 @@
+class Mathematics {
+  int _num = 0;
+  int _den = 0;
+
+  // void printFraction() {
+  //   print("$numerator/$denominator");
+  // }
+
+  // Setter Functions
+  void set numerator(int val) {
+    _num = val * 6;
+  }
+
+  void set denominator(int val) {
+    _den = val * 10;
+  }
+
+  //getter Functions
+  int get numerator => _num;
+
+  int get denominator => _den;
+}
+
+void main() {
+  Mathematics maths = new Mathematics();
+
+  maths.numerator = 5; //setter
+  maths.denominator = 6; //setter
+  print(maths.numerator); //getter
+  print(maths.denominator); //getter
+
+  // maths.printFraction();
+}
