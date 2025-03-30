dcc9e98f7b5e667daf768431485b3c4e636797eb
Alan-varghese <alanvz2212@gmail.com>
2025-03-30 11:41:09 +0400

    Updated

---
+++ 20.super_keyword.dart
@@ -0,0 +1,11 @@
+void main() {
+  MobilePhone mobilephone = new MobilePhone();
+}
+
+class Electronics {
+  double height = 2.0;
+  double weight = 6.0;
+  double thickness = 23.0;
+}
+
+class MobilePhone extends Electronics {}
