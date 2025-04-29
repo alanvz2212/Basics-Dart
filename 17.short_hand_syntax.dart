cd6152c14f75c0fd89cf5c5284a1255f23343949
Alan Varghese <alanvz757@gmail.com>
2025-04-29 10:20:25 +0400

    Updated

--- 17.short_hand_syntax.dart
+++ 17.short_hand_syntax.dart
@@ -1,31 +1,22 @@
-void main() {
-  int a = 1;
-  int b = 2;
-  int area = functionName(a, b);
-  print('The sum of two numbers is: $area');
-}
-
-int functionName(int a, int b) => a + b;
-
-// // void main() {
-// //   double l = 30.5;
-// //   double b = 45.9;
-// //   double area = areaRectangle(l, b);
-// //   print("Area of Rectangle is: $area");
-// // }
-
-// // double areaRectangle(double length, double breadth) {
-// //   double area = length * breadth;
-// //   return area;
-// // }
-
 // void main() {
-//   double l = 25.7;
-//   double b = 13.78;
-//   String s = areaRectangle(l, b);
-//   print(s);
-//   // print("The area of Rectangle is: $area");
+//   double l = 30.5;
+//   double b = 45.9;
+//   double area = areaRectangle(l, b);
+//   print("Area of Rectangle is: $area");
 // }
 
-// String areaRectangle(double length, double breadth) =>
-//     "The area of Rectangle is: ${length * breadth}";
+// double areaRectangle(double length, double breadth) {
+//   double area = length * breadth;
+//   return area;
+// }
+
+void main() {
+  double l = 25.7;
+  double b = 13.78;
+  String s = areaRectangle(l, b);
+  print(s);
+  // print("The area of Rectangle is: $area");
+}
+
+String areaRectangle(double length, double breadth) =>
+    "The area of Rectangle is: ${length * breadth}";
