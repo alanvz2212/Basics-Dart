1720bcaa4e209280fe12400562727258066b3e23
Alan-Varghese <alanvz2212@gmail.com>
2025-05-11 08:26:01 +0530

    Initial Commit

--- 18.inheritance_theory.dart
+++ 18.inheritance_theory.dart
@@ -16,3 +16,23 @@
 // Television extends mobile Phone -
 // Mobile Phone extends Electronics -
 // Electronics - height ,width,thickness,watch,AspectRatio,watch()
+
+// mobilephone extends Electronics = height, width, weight, aspecration, watch, call, sendmessages
+// Tv extends Electronics = height, weight, aspectration, watch;
+// Electronics = height, width, weight, aspectratio, watch;
+
+// mobilephone extends Electronics = height, width, weight, aspecration, watch, call, sendmessages
+// Tv extends Electronics = height, weight, aspectration, watch;
+// Electronics = height, width, weight, aspectratio, watch;
+
+
+
+
+
+
+// Inheritance
+// The copying of properties and behaviors from a parent class to a child class is called:
+// Copying" of parent class features by a child class = Inheritance
+
+//Single inheritance
+//Single Inheritance means a child class inherits from only one parent class. 
