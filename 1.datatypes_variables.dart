7a4f63d18a8ff7391d1c6734687a9df5898e7672
Alan Varghese <alanvz2212@example.com>
2025-05-15 16:21:25 +0530

    Updated

--- 1.datatypes_variables.dart
+++ 1.datatypes_variables.dart
@@ -1,39 +1,21 @@
-// //Datatypes and Variables
-// //flutter Tutorial
-// import 'dart:ffi';
-
-// void main() {
-//   int age = 22;
-//   double marks = 20.5;
-//   String name = "Alan";
-//   bool male = true;
-//   List<int> nums = [1, 2, 3, 4, 5];
-//   Set<String> fruits = {"Apple", "Mango"};
-//   Map<String, dynamic> myData = {
-//     "Age": age,
-//     "Marks": marks,
-//     "Name": name,
-//     "Gender": male,
-//   };
-//   print(nums);
-//   print(fruits);
-//   print(myData);
-// }
+//Datatypes and Variables
+//flutter Tutorial
+import 'dart:ffi';
 
 void main() {
   int age = 22;
+  double marks = 20.5;
   String name = "Alan";
-  double mark = 2.2;
   bool male = true;
-  Set<String> Alphabets = {'A', 'b', 'C'};
-  List<String> numbers = ['1', '2', '3', '4'];
-  Map<String, int> Data = {
+  List<int> nums = [1, 2, 3, 4, 5];
+  Set<String> fruits = {"Apple", "Mango"};
+  Map<String, dynamic> myData = {
     "Age": age,
+    "Marks": marks,
     "Name": name,
-    "mark": mark,
-    "Male": male,
-    "Alphabets":Alphabets,
-    "Numbers":numbers,
+    "Gender": male,
   };
-  print(Data);
+  print(nums);
+  print(fruits);
+  print(myData);
 }
