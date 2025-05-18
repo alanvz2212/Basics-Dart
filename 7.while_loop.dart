0e92868ecd7ac00025d46d54d68aacca493a15fe
Alan Varghese <alanvz2212@example.com>
2025-05-18 12:03:03 +0530

    Updated

--- 7.while_loop.dart
+++ 7.while_loop.dart
@@ -1,7 +1,14 @@
+// void main() {
+//   int i = 1;
+//   while (i <= 100) {
+//     print("Hello dart");
+//     i++;
+//   }
+// }
+
 void main() {
   int i = 1;
   while (i <= 100) {
-    print("Hello dart");
-    i++;
+    print(i);
   }
 }
