9a716de651112ea9bb99cca48339fedb1cb53bb9
Alan Varghese <alanvz2212@example.com>
2025-05-18 11:16:52 +0530

    Updated

--- 3.final_and_const.dart
+++ 3.final_and_const.dart
@@ -1,15 +1,15 @@
-void main() {
-  final String CompanyName = "Google";
-  const int CompanyEmployees = 50;
-  print(CompanyName);
-  print(CompanyEmployees);
-}
-
 // void main() {
 //   final String CompanyName = "Google";
-//   final dynamic numbers = 1;
-//   const int Employees = 50;
-//   print("My Company Name is :$CompanyName");
-//   print("only $numbers member in my company");
-//   print("$Employees employees in my company ");
+//   const int CompanyEmployees = 50;
+//   print(CompanyName);
+//   print(CompanyEmployees);
 // }
+
+void main() {
+  final String CompanyName = "Google";
+  final dynamic numbers = 1;
+  const int Employees = 50;
+  print("My Company Name is :$CompanyName");
+  print("only $numbers member in my company");
+  print("$Employees employees in my company ");
+}
--- 4.if-else.dart
+++ 4.if-else.dart
@@ -2,14 +2,14 @@
 
 // // if you are above the age of 18, then you are eliglble to vote
 
-void main() {
-  int age = 17;
-  if (age >= 18) {
-    print("you are eligible to vote");
-  } else {
-    print("You are not eligible to vote");
-  }
-}
+// void main() {
+//   int age = 17;
+//   if (age >= 18) {
+//     print("you are eligible to vote");
+//   } else {
+//     print("You are not eligible to vote");
+//   }
+// }
 // void main() {
 //   int age = 17;
 //   if (age <= 17) {
@@ -37,23 +37,23 @@ void main() {
 //   }
 // }
 
-// void main() {
-//   int age = 100;
-//   String vote = FunctionName(age);
-//   print(vote);
-// }
+void main() {
+  int age = 100;
+  String vote = FunctionName(age);
+  print(vote);
+}
 
-// String FunctionName(int age) {
-//   if (age >= 18 && age < 100) {
-//     return "You can vote";
-//   } else if (age <= 0) {
-//     return "Are you born ?";
-//   } else if (age >= 100) {
-//     return "You are not dead ?";
-//   } else {
-//     return "You cant Vote";
-//   }
-// }
+String FunctionName(int age) {
+  if (age >= 18 && age < 100) {
+    return "You can vote";
+  } else if (age <= 0) {
+    return "Are you born ?";
+  } else if (age >= 100) {
+    return "You are not dead ?";
+  } else {
+    return "You cant Vote";
+  }
+}
 
 
 
